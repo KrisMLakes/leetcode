e2f789942decb1589c483035032e59ce2351b531
KrisMLakes <168368756+KrisMLakes@users.noreply.github.com>
2025-08-02 16:08:40 -0400

    Time: 11 ms (49.63%), Space: 19.1 MB (28.94%) - LeetHub

---
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
@@ -0,0 +1,25 @@
+from collections import defaultdict
+class Solution:
+    def frequencySort(self, s: str) -> str:
+        freq = defaultdict(int)
+        arr = list(s)
+        s1=''
+        #print(arr)
+        for j in arr:
+            #print(j)
+            #print (freq[j])
+            freq[j]=freq[j]+1
+        arr1 = sorted(freq.values(), reverse=True)
+        #print(arr1)
+        while  arr1:# is not None:
+            j = arr1[0]
+            #print(j)
+            for k in list(freq):
+                if freq[k]==j:
+                    s1 = s1 + k*j
+                    #print(freq[k],k)
+                    del freq[k]
+            print(arr1)
+            arr1.remove(j)
+        return s1
+        
