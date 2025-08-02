c9455dd9e9dfd425665df6918254441b897345da
KrisMLakes <168368756+KrisMLakes@users.noreply.github.com>
2025-08-02 15:27:21 -0400

    Time: 0 ms (100%), Space: 17.9 MB (52.74%) - LeetHub

--- 1510-find-lucky-integer-in-an-array/1510-find-lucky-integer-in-an-array.py
+++ 1510-find-lucky-integer-in-an-array/1510-find-lucky-integer-in-an-array.py
@@ -1,14 +1,23 @@
 from collections import defaultdict
 class Solution:
     def findLucky(self, arr: List[int]) -> int:
+        arr1 = []
         freq = defaultdict(int)
         for i in arr:
             freq[i]= freq[i]+1
         lucky = -1
-        if not [freq[j] for j in freq if freq[j]==j]: # is None:
+        for j in freq:
+            if freq[j] == j:
+                arr1.append(j)
+
+        if len(arr1) == 0:
             return -1
         else:
-            return max([freq[j] for j in freq if freq[j]==j])
+            return max(arr1)
+        #if not [freq[j] for j in freq if freq[j]==j]: # is None:
+        #    return -1
+       # else:
+        #    return max([freq[j] for j in freq if freq[j]==j])
 
         #lucky = (if [freq[j] for j in freq if freq[j]==j] is None -1 else max[freq[j] for j in freq if freq[j]==j])
 
