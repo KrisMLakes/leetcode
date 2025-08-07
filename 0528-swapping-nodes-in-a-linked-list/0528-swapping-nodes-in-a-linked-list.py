0762ace4481c659623113853b1eaad839692be4b
KrisMLakes <168368756+KrisMLakes@users.noreply.github.com>
2025-08-07 13:35:54 -0400

    Time: 40 ms (60.95%), Space: 40.3 MB (31.57%) - LeetHub

--- 0528-swapping-nodes-in-a-linked-list/0528-swapping-nodes-in-a-linked-list.py
+++ 0528-swapping-nodes-in-a-linked-list/0528-swapping-nodes-in-a-linked-list.py
@@ -11,19 +11,19 @@ class Solution:
         slow = dummy.next
         prev = None
         val1 = 0
-        val2 = 0
+        #val2 = 0
         i = 0
         while i<k and fast.next:
             i = i + 1
             fast = fast.next
         val1 = fast.val
-        print(val1)
+        #print(val1)
         prev = fast
         while fast and fast.next:
             slow = slow.next
             fast = fast.next
         prev.val = slow.val
-        print(slow.val)
+        #print(slow.val)
         slow.val = val1
         
         return dummy.next
