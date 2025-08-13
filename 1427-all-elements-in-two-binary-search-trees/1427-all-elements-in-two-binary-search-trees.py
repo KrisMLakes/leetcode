13fa9df977d89c049ad6519892d223496ed3cf98
KrisMLakes <168368756+KrisMLakes@users.noreply.github.com>
2025-08-13 12:52:14 -0400

    Time: 125 ms (9.22%), Space: 21.7 MB (16.11%) - LeetHub

--- 1427-all-elements-in-two-binary-search-trees/1427-all-elements-in-two-binary-search-trees.py
+++ 1427-all-elements-in-two-binary-search-trees/1427-all-elements-in-two-binary-search-trees.py
@@ -7,28 +7,28 @@
 from collections import deque
 class Solution:
     def getAllElements(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> List[int]:
+        def inorder(node) -> List[int]:
+            return inorder(node.left) + [node.val] + inorder(node.right) if node else []
 
-        def getList(node) -> List[int]:
-            queue = deque([node])
-            ans = []
-            while queue:
-                num_nodes = len(queue)
-                for _ in range(num_nodes):
-                    node = queue.popleft()
-                    if node:
-                        ans.append(node.val)
-                        if node.left:
-                            queue.append(node.left)
-                        if node.right:
-                            queue.append(node.right)
-            return ans
-        
-        arr1 = getList(root1)
-        arr2 = getList(root2)
-        print(arr1, arr2)
-        combined = arr1 + arr2
-        combined.sort()
-        return combined
+        arr1 = inorder(root1)
+        arr2 = inorder(root2)
+
+        merged = []
+        i,j = 0,0
+        while i < len(arr1) and j < len(arr2):
+            if arr1[i] < arr2[j]:
+                merged.append(arr1[i])
+                i +=1
+            else:
+                merged.append(arr2[j])
+                j +=1
+            
+        merged += arr1[i:]
+        merged += arr2[j:]
+
+        return merged
+
+
 
 
         
