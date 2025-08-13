323cc460558c5751d36412d275a9db75e73f7c89
KrisMLakes <168368756+KrisMLakes@users.noreply.github.com>
2025-08-13 12:52:39 -0400

    Time: 116 ms (14%), Space: 21.7 MB (32.78%) - LeetHub

--- 1427-all-elements-in-two-binary-search-trees/1427-all-elements-in-two-binary-search-trees.py
+++ 1427-all-elements-in-two-binary-search-trees/1427-all-elements-in-two-binary-search-trees.py
@@ -4,7 +4,7 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-from collections import deque
+#from collections import deque
 class Solution:
     def getAllElements(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> List[int]:
         def inorder(node) -> List[int]:
