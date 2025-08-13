1dea5750c24ff36070cfb4c54bfa9c976434fe08
KrisMLakes <168368756+KrisMLakes@users.noreply.github.com>
2025-08-13 13:11:13 -0400

    Time: 47 ms (97.16%), Space: 21 MB (48.24%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -7,11 +7,20 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        if (p.val <= (root.val) <= q.val) or (q.val <= (root.val) <= p.val):
-            return root
-        elif (p.val <= root.val and q.val <= root.val):
-            return self.lowestCommonAncestor(root.left, p, q)
-        else:
-            return self.lowestCommonAncestor(root.right, p, q)
-
+    #    if (p.val <= (root.val) <= q.val) or (q.val <= (root.val) <= p.val):
+     #       return root
+      #  elif (p.val <= root.val and q.val <= root.val):
+       #     return self.lowestCommonAncestor(root.left, p, q)
+        #else:
+         #   return self.lowestCommonAncestor(root.right, p, q)
+        while root:
+            # Both nodes are smaller than root → go left
+            if p.val < root.val and q.val < root.val:
+                root = root.left
+            # Both nodes are larger than root → go right
+            elif p.val > root.val and q.val > root.val:
+                root = root.right
+            else:
+                # Split point found: root is between p and q → LCA
+                return root
         
