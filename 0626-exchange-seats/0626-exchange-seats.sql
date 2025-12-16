49cd8fa6104d0bcb893c77203606bad4209d119d
KrisMLakes <168368756+KrisMLakes@users.noreply.github.com>
2025-12-16 06:27:21 -0500

    Time: 365 ms (18.67%), Space: 0B (100%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -1,26 +1,9 @@
 # Write your MySQL query statement below
-select max(s3.id) as id, s3.student from 
-(select (S1.id-1) as id , S1.student
-from Seat S2, 
-(select id, student
-from Seat 
-where mod(id,2) = 0 ) S1
-where S1.id-1 = S2.id
-union
-select (S1.id+1) as id , S1.student
-from Seat S2, 
-(select id, student
-from Seat 
-where mod(id,2) = 1 ) S1
-where S1.id+1 = S2.id
-union
-select max(S5.id) as id, S5.student
-from Seat S5,
-(select max(id) as id, student
+select case 
+            when mod(id,2) = 1 and id = (select max(id) from Seat) then id
+            when mod(id,2) = 1 then id+1
+            else id-1  end
+            as id,
+            student
 from Seat
-where mod(id,2)=1) S6
-where S6.id = S5.id
-group by S5.student
-) S3
-group by S3.student
-order by S3.id 
+order by id 
