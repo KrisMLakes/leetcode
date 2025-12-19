8582e2f039b5f7c7444521ad63b7d78d8b434eec
KrisMLakes <168368756+KrisMLakes@users.noreply.github.com>
2025-12-19 06:00:01 -0500

    Time: 1068 ms (5.01%), Space: 0B (100%) - LeetHub

--- 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
+++ 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
@@ -1,11 +1,9 @@
-select id, sum(num1) as num from
-((select accepter_id as id , count(requester_id) num1
-from RequestAccepted 
-group by accepter_id)
-union all
-(select requester_id as id , count(accepter_id) num1
-from RequestAccepted 
-group by requester_id)) A
-group by A.id
-order by sum(A.num1) desc
-limit 1
+SELECT id, COUNT(*) AS num
+FROM (
+    SELECT accepter_id AS id FROM RequestAccepted
+    UNION ALL
+    SELECT requester_id AS id FROM RequestAccepted
+) t
+GROUP BY id
+ORDER BY num DESC
+LIMIT 1;
