b816b08574a681d958bcaeb7863002eff4a8d4f0
KrisMLakes <168368756+KrisMLakes@users.noreply.github.com>
2025-12-20 10:39:35 -0500

    Time: 5 ms (52.22%), Space: 24.6 MB (99.69%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -2,24 +2,17 @@ class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
         """
         Do not return anything, modify nums in-place instead.
-
-
+        2nd, chatgpt, 12/20/25
         """
         n = len(nums)
 
         k = k%n
-        
-
-        def reverse(left: int, right:int ) -> None:
-            while left < right:
-                nums[left], nums[right] = nums[right], nums[left]
-                left +=1
-                right -=1
-
+        def reverse(l, r):
+            while l <= r:
+                nums[l], nums[r] = nums[r], nums[l]
+                l +=1
+                r -=1
         reverse(0,n-1)
-
         reverse(0,k-1)
-
         reverse(k, n-1)
-
         
