0bd6a0b102f66fe6200021f74228bcaa840b844e
KrisMLakes <168368756+KrisMLakes@users.noreply.github.com>
2026-01-07 04:54:18 -0500

    Time: 3 ms (4.21%), Space: 19.5 MB (7.52%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -1,10 +1,24 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-##""2nd 12/17/2025"
+##""2nd 12/18/2025"
+## 3rd 1/7/2026
         k = 0
-        for i in range(len(nums)):
-            if nums[i] != val:
-                nums[k] = nums[i]
+        n = len(nums)
+        if n == 0:
+            return 0
+        i, j = 0, n-1
+        while i < n and i <= j:
+            if nums[i] == val and nums[j] != val:
+                print (nums[i], nums[j], "before", nums)
+                nums[i], nums[j] = nums[j], nums[i]
+                print (nums[i], nums[j], "after", nums)
+                i += 1
+                j -= 1
+                k += 1
+            elif nums[i] == val and nums[j] == val:
+                j -=1
+            elif nums[i] != val:
                 k +=1
-   
+                i +=1 
+            print (i, j, j)
         return k
