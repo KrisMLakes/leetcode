cbc0c0f34e7bf1616ccc8f7b66e3e8bc9076a44e
KrisMLakes <168368756+KrisMLakes@users.noreply.github.com>
2026-01-10 18:38:06 -0500

    Time: 0 ms (100%), Space: 20.3 MB (13.46%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,10 +1,16 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        #min_price = float('inf')
-        profit = 0
-        
-        for i in range(1, len(prices)): 
-            if prices[i] > prices[i-1]: # min_price:
-                profit += prices[i]-prices[i-1]
-        return profit
+        # The logic: If the price tomorrow is higher than today, "buy" today and "sell" tomorrow. If the price goes up again the day after, "buy" again and "sell" again. Adding up all these small gains results in the maximum possible total profit.
+
+        # 2nd, 1/10/26; saw logic trick
+        min_price = float('inf')
+        max_profit = 0
+        i = 1
+        while i < len(prices):
+
+            if prices[i] - prices[i-1] > 0:
+                max_profit += prices[i] - prices[i-1] 
+            i +=1
+        return max_profit
+
         
