f5d16f2d02964875998cf0f40e8c46a690409f69
KrisMLakes <168368756+KrisMLakes@users.noreply.github.com>
2026-01-10 17:24:55 -0500

    Time: 3 ms (83.8%), Space: 21.5 MB (6.78%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,18 +1,16 @@
 from collections import defaultdict
 class Solution:
     # 2nd 12/20/2025   dict example on google
+    # 3rd 1/10 dict example lookup
+    # in a coding interview, you would be expected to provide the Boyer-Moore solution to achieve constant space
     def majorityElement(self, nums: List[int]) -> int:
-        count = len(nums)
-        major_dict = {}
-        for i in nums:
-            if i in major_dict:
-
-                major_dict[i] = major_dict[i] + 1
-            else:
-                major_dict[i] = 1
-                
-            if major_dict[i] > count/2:
-                return i
+        count = 0
+        candidate = None
+        for num in nums:
+            if count == 0:
+                candidate = num
+            count +=(1 if candidate==num else -1)
+        return candidate
         
 
 
