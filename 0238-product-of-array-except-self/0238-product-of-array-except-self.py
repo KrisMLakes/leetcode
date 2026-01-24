5f6541539e78d945ea84952c8e26cf3d41fcc27c
KrisMLakes <168368756+KrisMLakes@users.noreply.github.com>
2026-01-24 11:25:15 -0500

    Time: 15 ms (96.04%), Space: 25.6 MB (39.38%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,26 +1,19 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
+        # 1/24 2nd: own solution in 21 minutes; missed the constraint, then saw solution
+        # idea: product of left * product of right
         n = len(nums)
-        answer = [0]*n
-        j = 0
-        product = 1
-        arr = []
-        for i in range(n):
-            if nums[i] == 0:
-                arr.append(i)
-                j +=1
-            else:
-                product *=nums[i]
-                #print (product)
-        print (j, product, arr)
-        if j > 1:
-            return answer
-        elif j == 1:
-            answer[arr[0]] = product
-            return answer
-        else:
-            for i in range(n):
-                answer[i] = int(product/nums[i])
-            return answer
 
+        answer = [1]*n
+
+        prefix = 1
+        for i in range(n):
+            answer[i] = prefix
+            prefix *= nums[i]
+        
+        suffix = 1
+        for i in range(n-1, -1, -1):
+            answer[i] *= suffix
+            suffix *= nums[i]
+        return answer
 
