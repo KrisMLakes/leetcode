664c7793439aa4d3d79ad9670da9074c48e4b638
KrisMLakes <168368756+KrisMLakes@users.noreply.github.com>
2026-01-29 07:21:12 -0500

    Time: 11 ms (61.14%), Space: 19.4 MB (34.67%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.py
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -1,18 +1,13 @@
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
-        zigrows = [""]*numRows
-        #print(zigrows)
-        if numRows == 1 or numRows > len(s):
+        if numRows == 1 or len(s) == 1:
             return s
-        cur_row, step = 0, -1
-
+        zigrows = [""]*numRows
+        step = -1
+        j = 0
         for i in s:
-            zigrows[cur_row] += i
-            #print(i)
-            if cur_row == 0 or cur_row == (numRows)-1:
+            zigrows[j] +=i
+            if j == 0 or j == numRows-1:
                 step *=-1
-            cur_row +=step
-            #print(zigrows)
-            #print(cur_row)
+            j +=step
         return "".join(zigrows)
-        
