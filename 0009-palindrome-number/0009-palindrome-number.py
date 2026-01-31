a736d0f40949fadff19d99922002d6109bde1c66
KrisMLakes <168368756+KrisMLakes@users.noreply.github.com>
2026-01-31 09:45:18 -0500

    Time: 147 ms (5.29%), Space: 19.4 MB (8.86%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -17,4 +17,29 @@ class Solution:
 
 
             
-        
+       # 2. Early exit optimization (common interview improvement)
+
+#You can avoid reversing the entire number. Instead, reverse only half of it.
+
+#Example:
+
+#1221
+
+#Left: 12
+
+#Right reversed: 12
+
+#This reduces work and avoids overflow issues in languages like Java/C++.
+
+#⭐ Industry-Standard Optimal Solution (Half Reversal)
+#class Solution:
+ #   def isPalindrome(self, x: int) -> bool:
+ #       if x < 0 or (x % 10 == 0 and x != 0):
+#            return False
+
+ #       rev = 0
+ #       while x > rev:
+ #           rev = rev * 10 + x % 10
+ #           x //= 10
+
+ #       return x == rev or x == rev // 10
