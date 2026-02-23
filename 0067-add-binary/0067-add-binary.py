84feade11e874df75ef30317d3785efcd7bb4fd9
KrisMLakes <168368756+KrisMLakes@users.noreply.github.com>
2026-02-23 08:54:03 -0500

    Time: 1 ms (51.59%), Space: 19.2 MB (88.52%) - LeetHub

--- 0067-add-binary/0067-add-binary.py
+++ 0067-add-binary/0067-add-binary.py
@@ -1,12 +1,21 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
+        # 2nd, chatgpt
+
         x = int(a,2)
         y = int(b,2)
 
         while y:
-            sum_ = x ^ y
-            carry = (x & y) << 1
-            x, y = sum_, carry
-        
+            sum1 = x ^ y
+            carry = x & y
+            y = carry << 1
+            x = sum1
         return bin(x)[2:]
+
+ #       while y:
+  #          sum_ = x ^ y
+  #          carry = (x & y) << 1
+   #         x, y = sum_, carry
+        
+   #     return bin(x)[2:]
         
